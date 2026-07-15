9e50975b2a1a221aa30a23f970380f306b7d9975
Nikhil-123vdot <nikhil.v0102@gmail.com>
2026-07-15 18:10:48 +0530

    Time: 79 ms (2.01%) | Memory: 29.6 MB (5.17%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        stack<int>st;
+        for(int i=0;i<s.size();i++)
+        {
+            st.push(s[i]);
+        }
+        int i=0;
+        while(! st.empty())
+        {
+            s[i]=st.top();
+            i++;
+            st.pop();
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            cout<<s[i];
+        }
+    }
+};
